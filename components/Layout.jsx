2e2589e43145dc1492b81e9d1f1d0ba771a560f2
tretrak <3trak10@gmail.com>
2021-12-23 13:43:10 +0100

    fix design2

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,21 @@
+import Head from "next/head";
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function Layout({ children }) {
   return (
-    <div className="container main-container">
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Yamna Recipe</title>
+        <meta name="description" content="Yamna Recipe" />
+      </Head>
+      <div className="container main-container">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 }
 
