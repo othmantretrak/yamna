9ea3838a7473e67287dcb905c3883dd35acd2759
tretrak <3trak10@gmail.com>
2021-12-23 02:52:15 +0100

    limit fixghg

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -5,8 +5,12 @@ export function useLocalStorage(key, initialValue) {
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
+      let item = "";
       // Get from local storage by key
-      const item = window.localStorage.getItem(key);
+      if (typeof window !== "undefined") {
+        // browser code
+        item = window.localStorage.getItem(key);
+      }
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
@@ -25,7 +29,10 @@ export function useLocalStorage(key, initialValue) {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== "undefined") {
+        // browser code
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
