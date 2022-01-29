7304936d019a30577f25ac19d436f2e8d81d7df0
tretrak <3trak10@gmail.com>
2022-01-29 19:26:53 +0100

    coming soon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,45 @@
 import { LoginContext } from "../context/LoginContext";
 import { useLocalStorage } from "../hooks/useLocalStorage";
-import "../styles/globals.css";
+import "../styles/css/style.css";
 
 function MyApp({ Component, pageProps }) {
-  const [name, setName] = useLocalStorage("name", "");
-  let isAdmin = name.isAdmin;
+  //const [name, setName] = useLocalStorage("name", "");
+  //let isAdmin = name.isAdmin;
   //console.log({ name });
   return (
-    <LoginContext.Provider value={isAdmin}>
-      <Component {...pageProps} />
+    <LoginContext.Provider>
+      <>
+        <div className="page">
+          <div className="countdown-col col">
+            <div className="time middle">
+              <span>
+                <div id="days">12</div> Days
+              </span>
+              <span>
+                <div id="hours">06</div> Hours
+              </span>
+              <span>
+                <div id="minutes">35</div> Minutes
+              </span>
+              <span>
+                <div id="seconds">54</div> Seconds
+              </span>
+            </div>
+          </div>
+          <div className="newsletter-col col">
+            <div className="newslatter middle">
+              <h2>We Will Coming Soon</h2>
+              <h4>Subscribe to get notification when we start</h4>
+              <form>
+                <input type="text" placeholder="Enter Your Email" />
+                <button type="button" className="newslatter-button">
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
     </LoginContext.Provider>
   );
 }
